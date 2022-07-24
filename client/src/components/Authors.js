d44507fe6625e274152d04fab9fef1a7c807e018
s-kakol <kakolsylwester@gmail.com>
2022-07-24 19:45:17 +0200

    Feat: Add basic client implementation with React

---
+++ client/src/components/Authors.js
@@ -0,0 +1,30 @@
+const Authors = props => {
+  if (!props.show) {
+    return null;
+  }
+  const authors = [];
+
+  return (
+    <div>
+      <h2>Authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Born</th>
+            <th>Books</th>
+          </tr>
+          {authors.map(a => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
