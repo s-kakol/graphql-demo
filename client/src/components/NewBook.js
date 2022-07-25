a23d4a681a6bb34a444b2d4e554285edaaa41218
s-kakol <kakolsylwester@gmail.com>
2022-07-25 17:45:37 +0200

    Feat: Add functionality in NewBook view

--- client/src/components/NewBook.js
+++ client/src/components/NewBook.js
@@ -1,4 +1,6 @@
+import { useMutation } from '@apollo/client';
 import { useState } from 'react';
+import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from '../queries';
 
 const NewBook = props => {
   const [title, setTitle] = useState('');
@@ -6,6 +8,9 @@ const NewBook = props => {
   const [published, setPublished] = useState('');
   const [genre, setGenre] = useState('');
   const [genres, setGenres] = useState([]);
+  const [addBook] = useMutation(ADD_BOOK, {
+    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
+  });
 
   if (!props.show) {
     return null;
@@ -14,7 +19,9 @@ const NewBook = props => {
   const submit = async event => {
     event.preventDefault();
 
-    console.log('add book...');
+    addBook({
+      variables: { title, author, published: Number(published), genres },
+    });
 
     setTitle('');
     setPublished('');
--- client/src/queries.js
+++ client/src/queries.js
@@ -19,3 +19,25 @@ export const ALL_BOOKS = gql`
     }
   }
 `;
+
+export const ADD_BOOK = gql`
+  mutation AddBook(
+    $title: String!
+    $published: Int!
+    $author: String!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      published: $published
+      author: $author
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`;
